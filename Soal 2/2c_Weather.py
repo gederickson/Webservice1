9b812b11af50fe5aeefa70b134df0058f514be11
Made Dia Agustya <dia.agustya@live.com>
2014-05-25 18:37:27 +0700

    Soal nomor 2c
    Web service cuaca Kota Jakarta

---
+++ Soal 2/2c_Weather.py
@@ -0,0 +1,36 @@
+import suds
+import xml.etree.ElementTree as etree
+
+url = 'http://www.webservicex.net/globalweather.asmx?WSDL'
+
+print 'Membuka WSDL: %s...' % url
+client = suds.client.Client(url)
+print 'OK'
+
+city = 'Jakarta'
+country = 'Indonesia'
+
+print 'Mengirim request cuaca untuk Kota %s Negara %s...' % (city, country)
+xml = client.service.GetWeather(city, country)
+print 'OK'
+
+
+xml = xml.encode('utf16')
+
+
+parse = etree.fromstring(xml)
+
+print '                             JAKARTA HARI INI'
+print '-------------------------------------------------------------------------------'
+print parse.find('Location').text
+print 'Per ', parse.find('Time').text
+print '-------------------------------------------------------------------------------'
+print 'Angin'.ljust(18), ': ', parse.find('Wind').text
+print 'Jarak Penglihatan'.ljust(18), ': ', parse.find('Visibility').text
+print 'Kondisi langit'.ljust(18), ': ', parse.find('SkyConditions').text
+print 'Suhu'.ljust(18), ': ', parse.find('Temperature').text
+print 'Titik Embun'.ljust(18), ': ', parse.find('DewPoint').text
+print 'Kelembapan'.ljust(18), ': ', parse.find('RelativeHumidity').text
+print 'Tekanan'.ljust(18), ': ', parse.find('Pressure').text
+
+print 'OK'
